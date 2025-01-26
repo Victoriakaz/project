c04c29e12de243187c7cb045481bc28c85ce1530
Victoria <victoriakaz23@mail.ru>
2025-01-26 20:57:56 +0300

    первый коммит

--- configuration.py
+++ configuration.py
@@ -1,9 +1,1 @@
-import requests
-import json  
-API_URL = "https://b3a9272a-dca8-48f9-9f90-3ff98708c161.serverhub.praktikum-services.ru"
-
-CREATE_USER_PATH = "/api/v1/users"
-KITS_PATH = "/api/v1/kits"
-
-
-
+ 
